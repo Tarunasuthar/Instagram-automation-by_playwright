d4240d1369dd9380fbbfa46bfefcfd7f9b3d5794
Tarunasuthar <120656706+Tarunasuthar@users.noreply.github.com>
2026-09-08 09:18:27 +0530

    Refactor Loginpage class for updated selectors

--- pages/Loginpage.js
+++ pages/Loginpage.js
@@ -1,24 +1,27 @@
 export class Loginpage {
   constructor(page) {
     this.page = page;
-    this.username = page.locator('[name="email"]');
-    this.password = page.locator('[name="pass"]');
-    this.submit = page.locator('button._aswp._aswr._aswu._asw_._asx2');
-    this.saveInfo = page.locator('button:has-text("Save info")');
+    this.username = page.locator('input[name="username"]');
+    this.password = page.locator('input[name="password"]');
+    this.submit = page.getByRole('button', { name: 'Log in', exact: true });
+    this.saveInfo = page.getByRole('button', { name: 'Save info' });
   }
 
   async goto() {
-    await this.page.goto("https://www.instagram.com/");
+    await this.page.goto('https://www.instagram.com/');
+    await this.username.waitFor({ state: 'visible' });
   }
-  // login in the instagram
+
   async login(user, pass) {
     await this.username.fill(user);
     await this.password.fill(pass);
     await this.submit.click();
   }
-  // save information of profile 
+
   async saveLoginInfo() {
-    await this.saveInfo.waitFor({ state: 'visible', timeout: 15000 });
-    await this.saveInfo.click();
+    // this dialog doesn't always appear
+    if (await this.saveInfo.isVisible({ timeout: 10000 }).catch(() => false)) {
+      await this.saveInfo.click();
+    }
   }
 }
